cb92191609eec0f5298f283f4127e225db86862e
Kydriaezh <bazanova.nata@gmail.com>
2023-04-04 11:19:25 +0300

    h

--- Seminar3/practice.java
+++ Seminar3/practice.java
@@ -16,5 +16,43 @@ public class practice {
 
 
     }
-    
+// написать метод в котором нужно убрать дубликаты.
+    public static void main(String[] args){
+        List<Integer> integers = new ArrayList<>(Array.asList(1, 1,2,3,1,5,3,5,6,9));
+        System.out.println(integers); 
+        remiveDuplicates(integers);
+        System.out.println(integers);
+
+    }
+    private static void removeDuplicates(List<Integer> source) {
+        int i = 0;
+        while (i<source.size()){
+            int j = i+1;
+            while (j, source.size()) {
+                while (j, source.size()) {
+                    if (source.get(i). equals(source.get(j))) {
+                        source.remove(j);
+                    }
+                    j++;
+                }
+                i++;
+            }
+
+        }
+    }
 }
+
+
+private static void removeDuplicates(List<Integer> source) {
+    List<Integer> duplicates = new ArrayList<>();
+    int i = 0;
+    while (i<source.size()){
+        Integer element = source.get(i);
+        if(!duplicates.contains(element)) {
+            duplicates.add(element);
+
+        }else {
+            source.remove(i--);
+        }
+        i++:
+    }
---
+++ Seminar4/practice.java
@@ -0,0 +1,9 @@
+package Seminar4;
+
+public class practice {
+    public static void main(String[] args) {
+        Person person = new Person (name: "Igor", age: "100");
+        System.out.println(person);
+  
+}
+}
