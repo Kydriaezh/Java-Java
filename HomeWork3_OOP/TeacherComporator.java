cc9e0238ac80c55cd1141bbbed2613c9b7785e64
Kydriaezh <bazanova.nata@gmail.com>
2023-04-25 13:01:14 +0300

    hwoop

---
+++ HomeWork3_OOP/TeacherComporator.java
@@ -0,0 +1,16 @@
+package HomeWork3_OOP;
+
+import java.util.Comparator;
+
+public class TeacherComporator implements Comparator<Human> {
+    public int compare(Human human, Human anotherHuman) {
+        if (human instanceof Teacher && anotherHuman instanceof Teacher) {
+            Teacher teacher = (Teacher) human;
+            Teacher anotherTeacher = (Teacher) anotherHuman;
+            return teacher.getStudentGroupSet().size() > anotherTeacher.getStudentGroupSet().size() ? -1
+                    : (teacher.getStudentGroupSet().size() < anotherTeacher.getStudentGroupSet().size() ? 1 : 0);
+        }
+        return 0; //There need IlligalArgumentException...
+    }
+}
+
