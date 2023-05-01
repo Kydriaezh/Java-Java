8e91f27c069c010de577da63c72a3ca021d35bbd
Kydriaezh <bazanova.nata@gmail.com>
2023-05-02 00:51:50 +0300

    hw6oop

---
+++ HomeWork6_OOP/repository/VendingMachineRepository.java
@@ -0,0 +1,12 @@
+package HomeWork6_OOP.repository;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import HomeWork6_OOP.model.Product;
+
+public class VendingMachineRepository {
+    static public List<Product> productList = new ArrayList<>();
+
+}
